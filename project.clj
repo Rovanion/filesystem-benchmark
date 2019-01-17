f567b3fedf55f484ac0d81049755b394f70275d2
Christian Luckey <christian.luckey.external@veoneer.com>
2019-01-17 16:35:02 +0100

    Remove unused dependencies and dead url.

--- project.clj
+++ project.clj
@@ -1,13 +1,11 @@
 (defproject filesystem-benchmark "0.1.0-SNAPSHOT"
   :description "A tiny program for benchmarking disk io"
-  :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure            "1.10.0"]
                  [org.clojure/tools.cli          "0.4.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [criterium "0.4.4"]
-                 [org.clojars.cantido/clj-mmap "2.1.0"]]
+                 [org.clojars.cantido/clj-mmap   "2.1.0"]]
   :main ^:skip-aot filesystem-benchmark.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
